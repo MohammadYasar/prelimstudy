0751cbb70f05ad495655de4cffbea64f1f80b2d8
MohammadYasar <yasar.samin@gmail.com>
2019-02-28 21:16:42 -0500

    added clipped portions

--- findcorr2.py
+++ findcorr2.py
@@ -9,7 +9,6 @@ class fcr:
     def __init__(self, _dir):
         self._dir =_dir
 
-#    def load
     def findCrossCorrelation(self, subject):
         corrVariables = ["/cartesian","/rotation"]
         kinematicsPath = self._dir + "/segments/" + subject + "/*"
@@ -47,23 +46,45 @@ class fcr:
             os.makedirs(covarPath)
         for variable in glob.glob(kinematicsPath):
             kin =  (variable.split("/")[len(variable.split("/"))-1])
-            covarList = []
+            covarList = {}
             for segment in glob.glob(variable+"/*"):
                 distrbution = []
                 segmentName = segment.split("/")[len(segment.split("/"))-1]
                 distrbution = externals.joblib.load(segment)
                 for i in range(1,len(experts)):
                     segment = segment.replace(experts[0], experts[i])
-                    print ("segmentName {} kinematics {} subject {}".format(segmentName, kin, experts[i]))
+                    #print ("segmentName {} kinematics {} subject {}".format(segmentName, kin, experts[i]))
                     if os.path.exists(segment):
                         distrbution = np.concatenate((distrbution, externals.joblib.load(segment)))
-                print ("distrbution shape {}".format(distrbution.shape))
+                #print ("distrbution shape {}".format(distrbution.shape))
                 left_covariance = np.cov(distrbution[:,0:distrbution.shape[1]/2], rowvar = False).reshape(-1,1)
+                left_temp = []
+                for left in left_covariance:
+                    left_temp.append(float(left[0]))
+                right_temp = []
                 right_covariance = np.cov(distrbution[:,distrbution.shape[1]/2:distrbution.shape[1]], rowvar = False).reshape(-1,1)
+                for right in right_covariance:
+                    right_temp.append(float(right[0]))
+                covarList[segmentName] =  [np.asarray(left_temp), np.asarray(right_temp)]
+                externals.joblib.dump(covarList, "{}/{}{}.p".format(covarPath, skill, kin))
 
-                covarList.append([segmentName, np.split(left_covariance, len(left_covariance)), np.split(right_covariance,len(left_covariance))])
-            covarList = pd.DataFrame(data  = covarList, columns = ["gestures", "left_covariance", "right_covariance"])
-            covarList.to_csv("{}/{}{}.csv".format(covarPath,skill, kin))
+
+    def compareCovariance(self):
+        covarPath = self._dir + "/covariances/csvs/"
+        kin = "grasperAngle"
+        skill = "intermediary"
+        ExpertCovDictLeft = externals.joblib.load("/home/uva-dsa1/Downloads/dVRKvideos/Needle_Passing/covariances/experts{}.p".format(kin))
+        NoviceCovDictLeft = externals.joblib.load("/home/uva-dsa1/Downloads/dVRKvideos/Needle_Passing/covariances/{}{}.p".format(skill,kin))
+        left_covariance = {}
+        right_covariance = {}
+        for key in ExpertCovDictLeft:
+            if key in NoviceCovDictLeft.keys():
+                left_covariance[key] =  ExpertCovDictLeft[key][1]-NoviceCovDictLeft[key][1]
+                right_covariance[key] =  ExpertCovDictLeft[key][1]-NoviceCovDictLeft[key][1]
+        _left = pd.DataFrame.from_dict(left_covariance)
+        _right = pd.DataFrame.from_dict(right_covariance)
+        _left.to_csv("{}/Left{}{}.csv".format(covarPath,skill, kin))
+        _right.to_csv("{}/Right{}{}.csv".format(covarPath,skill, kin))
 _dir = os.path.abspath(os.path.dirname(sys.argv[0]))
 fcr = fcr(_dir)
 #mpvds.loopSubjects()
@@ -72,7 +93,12 @@ experts = ['Needle_Passing_F001', 'Needle_Passing_F004', 'Needle_Passing_H005', 
 subjects = ['Needle_Passing_B003', 'Needle_Passing_B004', 'Needle_Passing_C001', 'Needle_Passing_C004', 'Needle_Passing_C005', 'Needle_Passing_D002', 'Needle_Passing_D003', 'Needle_Passing_D004', 'Needle_Passing_D005', 'Needle_Passing_E001','Needle_Passing_E003', 'Needle_Passing_E004', 'Needle_Passing_E005',  'Needle_Passing_F003', 'Needle_Passing_H002',  'Needle_Passing_I002', 'Needle_Passing_I003','Needle_Passing_I004']
 #subjects = ['Needle_Passing_C004']
 novices = ['Needle_Passing_B001', 'Needle_Passing_B002','Needle_Passing_C002', 'Needle_Passing_C003','Needle_Passing_D001',]
+"""
 fcr.gatherDistribution(subjects, "intermediary")
+fcr.gatherDistribution(experts, "experts")
+fcr.gatherDistribution(novices, "novices")
+"""
+fcr.compareCovariance()
 """
 for subject in subjects:
     fcr.findCrossCorrelation(subject)
---
+++ makevid.py
@@ -0,0 +1,40 @@
+import cv2, imageio, os, sys, glob, pandas, math
+import moviepy.editor as mpy
+
+class makeVideo:
+    def makeVideo(self, _dir):
+        globPath = _dir + "*"
+        fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+        images = []
+        images = [img for img in os.listdir(_dir)]
+        frame = cv2.imread(os.path.join(_dir, images[0]))
+        height, width, layers = frame.shape
+        video = cv2.VideoWriter("video.avi", fourcc,15, (width, height))
+        for name in sorted(glob.glob(globPath)):
+            video.write(cv2.imread(name))
+
+        cv2.destroyAllWindows()
+        video.release()
+
+    def clipVideo(self, clipName, start, end):
+        myclip = mpy.VideoFileClip(clipName)
+        print (myclip.fps) # prints for instance '30'
+        myclip2 = myclip.subclip(start, end)
+        myclip2.write_videofile(clipName.replace(".avi", "clipped.avi"), codec='mpeg4')
+        #myclip2.write_videofile(clipName.replace(".avi", "clipped.avi"))
+
+    def iterateoverFailures(self, _dir):
+        _filename = _dir + "/failure_times.csv"
+        _df = pandas.read_csv(_filename)
+        for index, row  in _df.iterrows():
+            _subject = _dir + "/video/"+ _df.at[index, 'Demonstration'] + "_capture1.avi"
+            if not math.isnan(_df.at[index, 'S1']):
+                _start = int(_df.at[index, 'S1'])
+                _end = int(_df.at[index, 'E1'])
+
+                self.clipVideo(_subject, _start, _end)
+
+_dir = os.path.abspath(os.path.dirname(sys.argv[0]))
+mkvd = makeVideo()
+#mkvd.makeVideo(_dir)
+mkvd.iterateoverFailures(_dir)
